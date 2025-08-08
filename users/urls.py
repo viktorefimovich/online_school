cd725122bebd0d2dbb8248cebc621e38d75b4773
Viktor Vershinin <viktorefimovich@bk.ru>
2025-08-08 11:33:06 +0700

    Исправлено

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
 router = DefaultRouter()
-router.register(r"profiles", UserViewSet)
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
-    path("", include(router.urls)),
+
 ]
+urlpatterns += router.urls
