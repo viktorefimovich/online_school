d466fd977912d4d28eb36d835c7f8fdef785fcea
Viktor Vershinin <viktorefimovich@bk.ru>
2025-08-08 11:20:09 +0700

    Реализованы CRUD для модели Lesson с помощью Generics

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("lms/", include("lms.urls", namespace="lms"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("lms/", include("lms.urls", namespace="lms"))]
