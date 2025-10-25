2070b5f3ed38d57f6b2140f31a254ac71771fc85
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-25 15:17:39 +0800

    Добавлена адмика платежей

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Payment
 
-# Register your models here.
+
+@admin.register(User)
+class MailingUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone", "city")
+    list_filter = list_display
+    search_fields = ("email", "phone", "city")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("user", "payment_date", "course", "lesson", "amount", "payment_method")
+    search_fields = ("user",)
+    list_filter = list_display
+    ordering = ['-payment_date']
