74c1e2e540c370bec6ce03d31d10e110c23de5bf
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-25 15:20:27 +0800

    Добавлена модель платежи

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -36,3 +38,22 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь",
+                             help_text="Укажите пользователя")
+    amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Сумма оплаты",
+                                 help_text="Укажите сумму оплаты")
+    payment_method = models.CharField(max_length=20, choices=[("cash", "Наличные"), ("transfer", "Перевод на счёт")],
+                                      verbose_name="Способ оплаты", help_text="Укажите способ оплаты")
+    payment_date = models.DateField(verbose_name="Дата оплаты", help_text="Укажите дату оплаты")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"Платеж {self.user}, сумма {self.amount}"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
