0a53d3d4c9ea355aac1b5334cb3787233766bcbb
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-27 13:57:17 +0800

    Исправлено

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
+
+from users.apps import UsersConfig
 from users.views import UserViewSet, PaymentViewSet
+
+app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
