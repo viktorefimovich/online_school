1f08ceaa9574f5e191903ef27f16d10a7c2ca8f4
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-27 13:44:22 +0800

    Исправлено

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import User
+from rest_framework.serializers import ModelSerializer
+
+from .models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
         fields = "__all__"
