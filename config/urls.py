d6bd29edc768852a468c2a4d695f077e6225f71c
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-27 13:54:35 +0800

    прописаны пути

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("lms/", include("lms.urls", namespace="lms"))
+    path("lms/", include("lms.urls", namespace="lms")),
+    path("users/", include("users.urls", namespace="users"))
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"payments", PaymentViewSet, basename="payments")
 
 urlpatterns = [
 
