f7bd14b55e20a5f8b82d463ca19c5a06f7bea531
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-27 13:16:56 +0800

    Исправлено

--- users/models.py
+++ users/models.py
@@ -1,7 +1,34 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from lms.models import Course, Lesson
+
+
+class CustomUserManager(BaseUserManager):
+    """Кастомный менеджер пользователей, где email является уникальным идентификатором"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Создает и возвращает пользователя с email и паролем"""
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Создает и возвращает суперпользователя с email и паролем"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -32,8 +59,10 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    objects = CustomUserManager()
+
     def __str__(self):
-        return self.username
+        return self.email
 
     class Meta:
         verbose_name = "Пользователь"
